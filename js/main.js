24158cd7494d935caef3e1c6b1bb03c9e4a9885b
Iza Duszyńska <iza.duszynska@wp.pl>
2020-09-06 12:31:05 +0200

    zadanie poprawione

--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
-var num1 = 4;
-var num2 = 2;
+var num1 = 2;
+var num2 = 5;
 
+function displayResults () {
 if(num1>num2) {
     console.log(num1);
 } else {
     console.log(num2)
 }
+}
+displayResults ();
