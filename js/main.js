e311613b2839f80be25cb3b0f02fa117b470fc2e
Iza Duszyńska <iza.duszynska@wp.pl>
2020-09-07 19:14:57 +0200

    poprawa1

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 var num1 = 2;
 var num2 = 5;
 
-function displayResults () {
-if(num1>num2) {
-    console.log(num1);
-} else {
-    console.log(num2)
+function displayResults() {
+    if (num1>num2) {
+        return(num1);
+    } else {
+        return(num2);
+    }
 }
-}
-displayResults ();
+console.log(displayResults());
